eceaccf0cbec4ac794ab169513a59d6f5d6528ff
martin76ec <larreaamartin@gmail.com>
2025-01-22 10:41:46 -0500

    feat: add openapi client generator script

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ import { SERVER_PORT_LOG } from "@providers/logs/logs-presets";
 import { v1 } from "@routes/v1";
 import Elysia from "elysia";
 
-const api = new Elysia({ prefix: "/api" });
-api.use(v1);
+const api = new Elysia({ prefix: "/api" }).use(v1);
 api.listen(REST_PORT, () => xlogger.info(SERVER_PORT_LOG, ["REST", REST_PORT]));
+
+//const x = treaty<typeof api>("");
+//const res = await x.api.v1.auth.login.post({
+//  email: "",
+//  password: "",
+//});
